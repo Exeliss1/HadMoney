a712f4f171f891f5ca398a65f968eb647adcbdd3
4831c0 <4831c0@proton.me>
2024-12-09 07:34:14 +0100

    Spinning wheel ft. ball

--- static/roulette.js
+++ static/roulette.js
@@ -3,6 +3,9 @@ let roulette_pill;
 let roulette_wheel;
 
 let baseSpinRate = 0.1;
+let ballRounds = 0;
+let ballStopRounds = 0;
+let ballStop = 0;
 
 function preload() {
     roulette_base = loadImage('roulette/roulette_base.png');
@@ -10,6 +13,9 @@ function preload() {
     roulette_wheel = loadImage('roulette/roulette_wheel.png');
 
     baseSpinRate += random(0.02, 0.1);
+    ballStopRounds = Math.floor(random(4, 8));
+    ballStop = random(0, Math.PI*2);
+    console.log(ballStopRounds, ballStop)
 }
 
 function setup() {
@@ -17,20 +23,74 @@ function setup() {
     canvas.id("p5-canvas");
 }
 
+let j = 0;
+let k = 0;
+let l = 1;
+
+const rvals = [0, 32, 15, 19, 4, 21, 2, 25, 17, 13, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 13, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26];
+const ranges = [];
+{
+    for (let m = 0; m < 37; m++) {
+        ranges.push(2*Math.PI/37*(m+0.5));
+    }
+}
+
+function ensureRange(n, n2) {
+    let n3 = n;
+    while (n3 >= n2) n3 -= n2;
+
+    return n3;
+}
+
+function ballLocation(angle, ballPos) {
+    for (let i = 0; i < ranges.length; i++) {
+        let j = i - 1;
+        if (j < 0) j = ranges.length-1;
+        const start = ensureRange(ranges[j] + angle, Math.PI*2);
+        const end = ensureRange(ranges[i] + angle, Math.PI*2);
+
+        if (start > end) {
+            if (ballPos > start || ballPos < end) {
+                return i;
+            }
+        } else {
+            if (ballPos >= start && ballPos <= end) return i;
+        }
+    }
+
+    debugger;
+}
+
 function drawWheel(angle) {
     const xOffset = 74+(452/2);
     const yOffset = 74+(452/2);
 
-    image(roulette_base, 0, 0);
-
     translate(xOffset, yOffset);
 
+    push();
     rotate(angle);
     imageMode(CENTER);
     image(roulette_wheel, 0, 0, 452, 452);
-    imageMode(CORNER);
+    pop();
 
+    push();
     translate(-xOffset, -yOffset);
+
+    imageMode(CORNER);
+    image(roulette_base, 0, 0);
+    pop();
+
+    imageMode(CENTER);
+
+    // TODO: Make scaling smoother
+    // scale(1 - (0.35 * (ballRounds / ballStopRounds)));
+    rotate(j);
+    if (j < 0) {
+        j = Math.PI*2;
+        ballRounds++;
+        console.log(ballRounds);
+    }
+    image(roulette_pill, 0, 0);
 }
 
 let i = 0;
@@ -46,12 +106,16 @@ function draw() {
     noStroke();
     tint(255, 255);
 
-    let fps = frameRate();
-    text(fps, 50, 50);
+    // frameRate()
+    let debug = `${i} ${j} ${rvals[ballLocation(i, j)]}`;
+    text(debug, 50, 50);
 
     if (wheelSpinState < 1) wheelSpinState += random(0.004, 0.01);
     if (wheelSpinState > 1) wheelSpinState = 1;
     i += (baseSpinRate * (1 - animate(wheelSpinState)));
+    if (ballRounds < ballStopRounds || Math.abs(j - ballStop) > 0.1) {
+        j -= 0.05;
+    }
     if (i >= 6.283) i -= 6.283;
 
     push();
